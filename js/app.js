2699f7d58aa58826f3b0bcd5b877ce72e827d27a
Martin Silha <martysilha@gmail.com>
2020-11-03 23:14:15 -0600

    click and render complete

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 
 /* 	1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1. */
 const players = {
-  '1': 'purple',
-  '-1': 'lime',
-  'null': 'gainsboro'
+  '1': 'cadetblue',
+  '-1': 'darkgoldenrod',
+  'null': 'azure'
 };
 
 /* 1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value. */
@@ -44,7 +44,7 @@ function init() {
   board = new Array(9).fill(null); // 4.1.1) Initialize the board array to 9 nulls to represent empty squares. The 9 elements will "map" to each square, where index 0 maps to the top-left square and index 8 maps to the bottom-right square.
   turn = 1; // 4.1.2) Initialize whose turn it is to 1 (player 'X'). Player 'O' will be represented by -1.
 	winner = null; // 4.1.3) Initialize winner to null to represent that there is no winner or tie yet. Winner will hold the player value (1 or -1) if there's a winner. Winner will hold a 'T' if there's a tie.
-	// render();
+	render();
 }
 
 init();
@@ -55,7 +55,7 @@ function render() {
   board.forEach(function(sqr, idx) {// 4.2.1.1) Loop over each of the 9 elements that represent the squares on the page, and for each iteration:    
   console.log(idx);// 4.2.1.1.2) Use the index of the iteration to access the mapped value from the board array. 
 	sqrs[idx].style.background = players[sqr]; // 4.3.1.1.3) Set the background color of the current element by using the value as a key on the colors lookup object (constant). 
-    // 4.2.2) Render a message:
+  // 4.2.2) Render a message:
   if (winner === 'T') {msg.innerText = `Cat's game!`;// 4.2.2.2) If winner is equal to 'T' (tie), render a tie message.
   } else if (winner) {`Congratulations, ${players[winner].toUpperCase()}...you did it!`; // 4.2.2.3) Otherwise, render a congratulatory message to which player has won - use the color name for the player, converting it to uppercase.
   } else {
@@ -65,19 +65,14 @@ function render() {
 });
 }
 
-function Click() {
-	console.log("Click!");
-}
-
 // 5) Handle a player clicking a square:
 function getClick(evt) {
-  const idx = evt.target.id;// 5.1) Obtain the index of the square that was clicked by either:
-  // 		5.1.1) "Extracting" the index from an id assigned to the element in the HTML, or
+  const idx = evt.target.id;// 5.1) Obtain the index of the square that was clicked by either: "Extracting" the index from an id assigned to the element in the HTML, or
   // 		5.1.2) Looping through the cached square elements using a for loop and breaking out when the current square element equals the event object's target.
   if (board[idx] || winner) return; // 5.2) If the board has a value at the index, immediately return because that square is already taken. // 5.3) If winner is not null, immediately return because the game is over.
   board[idx] = turn; // 5.4) Update the board array at the index with the value of turn.
   turn *= -1; // 5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
-  console.log(idx);
+  render();
 }
 
 function getWinner() {
